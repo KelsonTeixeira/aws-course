cfb7dc632bfdddb95d399b168840633f073d7914
kelsonteixeira <txkelson@gmail.com>
2026-02-11 02:02:58 -0300

    lambda class commit

---
+++ lambda/src/infra/Utils.ts
@@ -0,0 +1,7 @@
+import { Fn, Stack } from "aws-cdk-lib";
+
+export function getSuffixFromStack(stack: Stack) {
+    const shorStackID = Fn.select(2, Fn.split('/', stack.stackId));
+    const stackSuffix = Fn.select(4, Fn.split('-', shorStackID));
+    return stackSuffix;
+  }
